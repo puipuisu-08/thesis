cd71666360d0c4adb0dcdb8552b1aac69dace815
chinezeboi-08 <71803948+chinezeboi-08@users.noreply.github.com>
2023-08-29 12:44:51 +0700

    use weather_25K

--- scripts/PatchTST/script.sh
+++ scripts/PatchTST/script.sh
@@ -11,7 +11,7 @@ domain_classifier=DANN_Default
 
 root_path_name=/kaggle/input/patchtst-dataset/
 source_path_name=electricity_25K.csv
-target_path_name=weather_5K.csv
+target_path_name=weather_25K.csv
 model_id_name=v1
 data_name=custom
 
@@ -44,7 +44,7 @@ do
       --train_epochs 100\
       --patience 20\
       --num_workers 10\
-      --num_sources 5\
+      --num_sources 1\
       --num_features 321\
       --itr 1 --batch_size 16 --learning_rate 0.0001
 done
